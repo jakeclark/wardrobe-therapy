4d100e34e4be2f2fa963bce06d5ad470b9a4a540
Conor Barr <conor.barr@lightwellinc.com>
2015-04-06 17:02:49 -0400

    Conor's Changes

--- app/models/lookbook.rb
+++ app/models/lookbook.rb
@@ -2,7 +2,7 @@ class Lookbook < ActiveRecord::Base
 
   has_and_belongs_to_many :users
   has_and_belongs_to_many :books
-  Paperclip.options[:command_path] = 'C:/Program Files/ImageMagick-6.9.0-Q16'
+  #Paperclip.options[:command_path] = 'C:/Program Files/ImageMagick-6.9.0-Q16'
 
   has_attached_file :photo, :styles => { :small => "150x150" , :medium => "250x250"}
   has_attached_file :photo_extra, :styles => { :small => "150x150" , :medium => "250x250"}
